28c93d15c4408c8af1e48f79fe4c6070c9abbd84
Punal Chotrani <punalsc@gmail.com>
2016-09-08 17:06:00 +0100

    updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,48 @@ module.exports = function (grunt) {
                     helpers: 'js/spec/*Helper.js'
                 }
             }
+        },
+        sass: {
+            options: {
+                sourcemap: 'none'
+            },
+            dist: {
+                files: {
+                    'css/base-sass.css': 'css/sass/base-sass.scss'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'css/base-sass.css.min': ['css/base-sass.css']
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: 'js/*.js',
+                tasks: ['qunit', 'concat', 'uglify']
+            },
+            css: {
+                files: 'css/sass/*.scss',
+                tasks: ['sass', 'cssmin']
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
     // Load the plugins that provide the tasks.
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    //Default tasks
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
 
 
 };
