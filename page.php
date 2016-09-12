402fd93d36605d11c5f5c94f36cc5042b4c465eb
Punal Chotrani <punalsc@gmail.com>
2016-09-12 17:50:51 +0100

    added thumbnail images

--- page.php
+++ page.php
@@ -24,23 +24,33 @@
 					</div>
 				</div>
 			</div>
-			<div class="container parent-content">
+			<section class="container parent-content">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<?php the_content(); ?>
 				<?php endwhile; endif; ?>
-			</div>
-			<div class="container">
+			</section>
+			<section class="container">
 				<div class="col-md-6">
-					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</p>
+					<div class="thumbnail">
+						<img src="..." alt="...">
+						<div class="caption">
+							<h3>Thumbnail label</h3>
+							<p>...</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						</div>
+					</div>
 				</div>
 				<div class="col-md-6">
-					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-					</p>
+					<div class="thumbnail">
+						<img src="..." alt="...">
+						<div class="caption">
+							<h3>Thumbnail label</h3>
+							<p>...</p>
+							<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+						</div>
+					</div>
 				</div>
-			</div>
+			</section>
 		</div>
 	</main>
 <?php get_footer(); ?>
