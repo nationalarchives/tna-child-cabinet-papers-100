d752bfe17349a3cc6abdff5902189b7479f060c4
Punal Chotrani <punalsc@gmail.com>
2016-09-13 14:31:56 +0100

    added bookshop and related link boxes

--- page.php
+++ page.php
@@ -28,24 +28,84 @@
 			<section class="container">
 				<div class="col-md-6 cabinet-documents">
 					<div class="caption">
-						<h2>Thumbnail label</h2>
+						<a href="#">
+							<h2>Thumbnail label</h2>
+						</a>
 						<p>
 							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
 						</p>
 					</div>
 					<div class="thumbnail">
-						<img src="http://www.nationalarchives.gov.uk/wp-content/uploads/2015/10/feature-agincourt-Henry-V-seal-720x553.jpg" alt="Why the battle of Agincourt happened">
+						<a href="#"><img src="http://www.nationalarchives.gov.uk/wp-content/uploads/2015/10/feature-agincourt-Henry-V-seal-720x553.jpg" alt="Why the battle of Agincourt happened"></a>
 					</div>
 				</div>
 				<div class="col-md-6 cabinet-documents">
 					<div class="caption">
-						<h2>Thumbnail label</h2>
+						<a href="#">
+							<h2>Thumbnail label</h2>
+						</a>
 						<p>
 							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
 						</p>
 					</div>
 					<div class="thumbnail">
-						<img src="http://www.nationalarchives.gov.uk/wp-content/uploads/2015/10/Feature-pouch-of-Sir-Simon-Felbrigge-720x553.jpg" alt="Preparing to fight: Raising soldiers and supplies">
+						<a href="#"><img src="http://www.nationalarchives.gov.uk/wp-content/uploads/2015/10/Feature-pouch-of-Sir-Simon-Felbrigge-720x553.jpg" alt="Preparing to fight: Raising soldiers and supplies"></a>
+					</div>
+				</div>
+				<div class="col-md-6 cabinet-documents">
+					<div class="caption">
+						<a href="#">
+							<h2>Thumbnail label</h2>
+						</a>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+						</p>
+					</div>
+					<div class="thumbnail">
+						<a href="#"><img src="http://www.nationalarchives.gov.uk/wp-content/uploads/2015/10/feature-agincourt-Henry-V-seal-720x553.jpg" alt="Why the battle of Agincourt happened"></a>
+					</div>
+				</div>
+				<div class="col-md-6 cabinet-documents">
+					<div class="caption">
+						<a href="#">
+							<h2>Thumbnail label</h2>
+						</a>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+						</p>
+					</div>
+					<div class="thumbnail">
+						<a href="#"><img src="http://www.nationalarchives.gov.uk/wp-content/uploads/2015/10/feature-agincourt-Henry-V-seal-720x553.jpg" alt="Why the battle of Agincourt happened"></a>
+					</div>
+				</div>
+			</section>
+			<section class="container">
+				<div class="col-md-6">
+					<div class="thumbnail">
+						<p>
+							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+						</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="thumbnail">
+						<p>
+							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+						</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="thumbnail">
+						<p>
+							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+						</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="thumbnail">
+						<p>
+							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
+						</p>
 					</div>
 				</div>
 			</section>
