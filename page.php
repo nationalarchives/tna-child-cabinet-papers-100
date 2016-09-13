3c410f8b2cd0e230e97f87ffaadec5b9cc7cecbf
Punal Chotrani <punalsc@gmail.com>
2016-09-13 15:03:51 +0100

    more css and layout fixes

--- page.php
+++ page.php
@@ -79,9 +79,10 @@
 					</div>
 				</div>
 			</section>
-			<section class="container">
+			<section class="container boxes">
 				<div class="col-md-6">
 					<div class="thumbnail">
+						<h2>Bookshop</h2>
 						<p>
 							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
 						</p>
@@ -89,20 +90,7 @@
 				</div>
 				<div class="col-md-6">
 					<div class="thumbnail">
-						<p>
-							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
-						</p>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="thumbnail">
-						<p>
-							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
-						</p>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="thumbnail">
+						<h2>Related links</h2>
 						<p>
 							is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
 						</p>
