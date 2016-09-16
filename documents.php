1f439dd4e273306ec14b4962ef8aad564a30eecc
Punal Chotrani <punalsc@gmail.com>
2016-09-16 11:22:31 +0100

    fixed padding and margin issues on document pages.

--- documents.php
+++ documents.php
@@ -6,27 +6,29 @@ get_header();
 get_template_part( 'breadcrumb' ); ?>
 <main>
 	<section class="container">
-		<h1><?php the_title() ?></h1>
-		<div class="document-viewer">
-			<?php
-			$page_id = $post->ID;
-			$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
-			$childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
-			?>
-			<a href="<?php echo make_path_relative_no_pre_path($image[0]); ?>" target="_blank">
-				<div style="background-image: url('<?php
-				if (has_post_thumbnail($page_id)) {
-					echo make_path_relative_no_pre_path($image[0]);
-				}
-				?>')"></div>
-			</a>
-			<div class="overlay">
-				<a target="_blank" class="button align-right" href="<?php echo make_path_relative_no_pre_path($image[0]); ?>">View full image</a>
+		<div class="row">
+			<h1><?php the_title() ?></h1>
+			<div class="document-viewer">
+				<?php
+				$page_id = $post->ID;
+				$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
+				$childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
+				?>
+				<a href="<?php echo make_path_relative_no_pre_path($image[0]); ?>" target="_blank">
+					<div style="background-image: url('<?php
+					if (has_post_thumbnail($page_id)) {
+						echo make_path_relative_no_pre_path($image[0]);
+					}
+					?>')"></div>
+				</a>
+				<div class="overlay">
+					<a target="_blank" class="button align-right" href="<?php echo make_path_relative_no_pre_path($image[0]); ?>">View full image</a>
+				</div>
 			</div>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; endif; ?>
 		</div>
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; endif; ?>
 	</section>
 
 	<?php
@@ -45,31 +47,35 @@ get_template_part( 'breadcrumb' ); ?>
 	if ( $the_query->have_posts() ) : ?>
 	<!-- while ( $the_query->have_posts() ) : $the_query->the_post(); -->
 	<section class="container explore-records">
-		<h2>Explore the records</h2>
-		<span id="slider-prev"></span>
-		<span id="slider-next"></span>
-			<div class="bxslider">
-				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-				<a href="<?php echo get_page_link($page->ID); ?>">
-					<div class="document-slide-thumb" style="background-image: url('<?php
-					$page_id = $post->ID;
-					$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
-					$childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
-					if (has_post_thumbnail($page_id)) {
-						echo make_path_relative_no_pre_path($image[0]);
-					} elseif (is_page($page_id)) {
-						echo make_path_relative_no_pre_path($childimage[0]);
-					}
-					?>')">
-					</div>
-					<div><p><?php the_title(); ?></p></div>
-				</a>
-				<?php endwhile; ?>
-			</div>
+		<div class="row">
+			<h2>Explore the records</h2>
+			<span id="slider-prev"></span>
+			<span id="slider-next"></span>
+				<div class="bxslider">
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+					<a href="<?php echo get_page_link($page->ID); ?>">
+						<div class="document-slide-thumb" style="background-image: url('<?php
+						$page_id = $post->ID;
+						$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
+						$childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
+						if (has_post_thumbnail($page_id)) {
+							echo make_path_relative_no_pre_path($image[0]);
+						} elseif (is_page($page_id)) {
+							echo make_path_relative_no_pre_path($childimage[0]);
+						}
+						?>')">
+						</div>
+						<div><p><?php the_title(); ?></p></div>
+					</a>
+					<?php endwhile; ?>
+				</div>
+		</div>
 	</section>
 	<?php endif; wp_reset_postdata(); ?>
 	<section class="container">
-		<a class="button align-right" href="<?php echo get_site_url(); ?>">Return to the Battle of Agincourt</a>
+		<div class="row">
+			<a class="button align-right" href="<?php echo get_site_url(); ?>">Return to the Battle of Agincourt</a>
+		</div>
 	</section>
 </main>
 <?php get_footer(); ?>
