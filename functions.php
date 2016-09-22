0d6885541cbd9b55526c2fa6ae098a8004af41bd
Punal Chotrani <punalsc@gmail.com>
2016-09-22 17:44:52 +0100

    adding additional flexible meta boxes.

--- functions.php
+++ functions.php
@@ -49,3 +49,66 @@ function theme_setup() {
 		add_image_size( 'custom-image', 612, 470, array('center','center') );
 	}
 }
+
+//Adding the custom field for the  Additional boxes.
+function additional_meta () {
+	global $post;
+	$pageTemplate = get_post_meta($post->ID, '_wp_page_template', true);
+	if ($pageTemplate == 'page-cabinet-template.php') {
+		add_meta_box (
+			'additional_meta',
+			'Additional Content',
+			'additional_meta_callback',
+			'page',
+			'normal',
+			'default'
+		);
+	}
+}
+add_action('add_meta_boxes', 'additional_meta');
+function additional_meta_callback( $post ) {
+	wp_nonce_field(basename( __FILE__), 'additional_meta_monce');
+	$additional_stored_meta = get_post_meta ($post->ID); ?>
+	<div class="wrap">
+		<h4 style="color: #009999; font-size: 15px;">Left column section</h4>
+		<div class="meta-row">
+			<div class="meta-td">
+				<input placeholder="Enter the title for the left hand column" class="widefat" type="text" name="Left hand column title" id="Left hand column title" value=""/>
+			</div>
+		</div>
+		<div class="meta-row">
+			<div class="meta-th" style="margin-top: 2em;">
+				<span><strong>Add content for left column box</strong></span>
+			</div>
+		</div>
+		<div class="meta-editor" style="margin-top: 1em;">
+			<?php
+			$content = get_post_meta( get_the_id(), 'left_content', true );
+			$editor_id = 'left_content';
+			$settings = array(
+				'quicktags' => array( 'buttons' => 'strong,em,del,ul,ol,li,close' ),
+				'textarea_rows' => 8
+			);
+			wp_editor( $content, $editor_id, $settings );
+			?>
+		</div>
+		<hr style="margin-top: 2.5em;">
+		<h4 style="color: #009999; font-size: 15px;">Right column section</h4>
+		<div class="meta-row">
+			<div class="meta-td">
+				<input placeholder="Enter the title for the right hand column" class="widefat" type="text" name="Right hand column title" id="Right hand column title" value=""/>
+			</div>
+		</div>
+		<div class="meta-editor" style="margin-top: 1em;">
+			<?php
+			$content = get_post_meta( get_the_id(), 'right_content', true );
+			$editor_id = 'right_content';
+			$settings = array(
+				'quicktags' => array( 'buttons' => 'strong,em,del,ul,ol,li,close' ),
+				'textarea_rows' => 8
+			);
+			wp_editor( $content, $editor_id, $settings );
+			?>
+		</div>
+	</div>
+<?php } ?>
--- page-cabinet-template.php
+++ page-cabinet-template.php
@@ -25,15 +25,17 @@ get_header(); ?>
 					</div>
 				</div>
 			</div>
+			<?php if (have_posts()) : ?>
 			<section class="container parent-content">
 				<div class="row">
 					<div class="col-md-12">
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<?php while (have_posts()) : the_post(); ?>
 							<?php the_content(); ?>
-						<?php endwhile; endif; ?>
+						<?php endwhile; ?>
 					</div>
 				</div>
 			</section>
+			<?php endif; ?>
 			<section class="container">
 				<div class="row">
 					<?php
