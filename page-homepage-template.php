eee066184894e971ab5e6968fae7bb6ac4a6405d
Punal Chotrani <punalsc@gmail.com>
2016-10-03 16:35:59 +0100

    added new h1 styling, eye caption fix to top.

--- page-homepage-template.php
+++ page-homepage-template.php
@@ -20,7 +20,7 @@ get_header(); ?>
 					<div class="row">
 						<div class="col-md-12">
 							<?php get_image_caption('top'); ?>
-							<h1><span><?php echo get_the_title(); ?></span></h1>
+							<h1 class="super-heading"><?php echo get_the_title(); ?></h1>
 						</div>
 					</div>
 				</div>
