4c187d2ad0167c4bbd7c23a852ec79f1ed3c873a
Punal Chotrani <punalsc@gmail.com>
2019-08-23 15:55:38 +0100

    Fixed template styling

--- page-homepage-template.php
+++ page-homepage-template.php
@@ -3,8 +3,8 @@
 Template Name: Homepage template
 */
 get_header(); ?>
-	<main role="main">
-		<div class="banner feature-img" style="background-image: url('<?php
+<main role="main">
+    <div class="banner feature-img" style="background-image: url('<?php
 		$page_id = $post->ID;
 		$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
 		$childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
@@ -14,31 +14,34 @@ get_header(); ?>
 			echo make_path_relative_no_pre_path($childimage[0]);
 		}
 		?>')">
-			<?php get_template_part('breadcrumb'); ?>
-			<div class="heading-banner text-left">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<?php get_image_caption('top'); ?>
-							<h1 class="super-heading"><?php echo get_the_title(); ?></h1>
-						</div>
-					</div>
-				</div>
-			</div>
-			<?php if (have_posts()) : ?>
-			<section class="container parent-content">
-				<div class="row">
-					<div class="col-md-12">
-						<?php while (have_posts()) : the_post(); ?>
-							<?php the_content(); ?>
-						<?php endwhile; ?>
-					</div>
-				</div>
-			</section>
-			<?php endif; ?>
-			<section class="container">
-				<div class="row">
-					<?php
+        <?php get_template_part('breadcrumb'); ?>
+        <section class="container heading-banner text-left">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php get_image_caption('top'); ?>
+                    <div class="entry-header">
+                        <h1 class="super-heading"><?php echo get_the_title(); ?></h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <?php if (have_posts()) : ?>
+        <section class="container">
+            <div class="row">
+                <div class="col-md-12 content-wrapper">
+                    <?php while (have_posts()) : the_post(); ?>
+                    <?php the_content(); ?>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+        </section>
+        <?php endif; ?>
+
+        <section class="container">
+            <div class="row">
+                <?php
 					$parent_id = get_the_ID();
 					$args = array(
 						'category_name' => 'homepage',
@@ -51,19 +54,22 @@ get_header(); ?>
 					if ( $the_query->have_posts() ) :
 					while ( $the_query->have_posts() ) : $the_query->the_post();
 					?>
-						<div class="col-md-6">
-							<div class="cabinet-documents">
-								<div class="thumbnail">
-									<?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
-									<a href="<?php echo make_path_relative(get_page_link()); ?>" title="<?php echo get_the_title(); ?>">
-										<img src="<?php echo make_path_relative($image_url[0]); ?>" class="img-responsive" alt="<?php echo $post->post_title ?>">
-									</a>
-								</div>
-								<div class="caption">
-									<a href="<?php echo make_path_relative(get_page_link()); ?>" title="<?php echo get_the_title(); ?>">
-										<h2><?php echo get_the_title(); ?></h2>
-									</a>
-									<?php
+                <div class="col-md-6">
+                    <div class="cabinet-documents">
+                        <div class="thumbnail">
+                            <?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
+                            <a href="<?php echo make_path_relative(get_page_link()); ?>"
+                                title="<?php echo get_the_title(); ?>">
+                                <img src="<?php echo make_path_relative_no_pre_path($image_url[0]); ?>"
+                                    class="img-responsive" alt="<?php echo $post->post_title ?>">
+                            </a>
+                        </div>
+                        <div class="caption">
+                            <a href="<?php echo make_path_relative(get_page_link()); ?>"
+                                title="<?php echo get_the_title(); ?>">
+                                <h2><?php echo get_the_title(); ?></h2>
+                            </a>
+                            <?php
 									if (has_excerpt()) {
 										the_excerpt();
 									}
@@ -71,41 +77,42 @@ get_header(); ?>
 										echo first_sentence(get_the_content());
 									}
 									?>
-								</div>
-							</div>
-						</div>
-					<?php endwhile; endif; ?>
-				</div>
-			</section>
-			<?php
+                        </div>
+                    </div>
+                </div>
+                <?php endwhile; endif; ?>
+            </div>
+        </section>
+        <?php
 			$left_title = get_post_meta($page_id, 'left_column_title', true);
 			$left_content = get_post_meta($page_id, 'left_content', true);
 			$right_title = get_post_meta($page_id, 'right_column_title', true);
 			$right_content = get_post_meta($page_id, 'right_content', true);
 			if ( !empty($left_title) || !empty($left_content)  ) : ?>
-			<section class="container">
-				<div class="row">
-					<div class="<?php if ( empty($right_title) || empty($right_content ) ) { echo 'col-md-12'; } else { echo 'col-md-6'; } ?>">
-						<div class="thumbnail end-box">
-							<h2><?php echo $left_title; ?></h2>
-							<div class="img-box">
-								<?php echo wpautop($left_content); ?>
-							</div>
-						</div>
-					</div>
-					<?php if( !empty($right_content) || !empty($right_content) ) : ?>
-					<div class="col-md-6">
-						<div class="thumbnail end-box">
-							<h2><?php echo $right_title; ?></h2>
-							<div class="img-box">
-								<?php echo wpautop($right_content); ?>
-							</div>
-						</div>
-					</div>
-					<?php endif; ?>
-				</div>
-			</section>
-			<?php endif; ?>
-		</div>
-	</main>
+        <section class="container">
+            <div class="row">
+                <div
+                    class="<?php if ( empty($right_title) || empty($right_content ) ) { echo 'col-md-12 content-wrapper'; } else { echo 'col-md-6 content-wrapper'; } ?>">
+                    <div class="thumbnail end-box">
+                        <h2><?php echo $left_title; ?></h2>
+                        <div class="img-box">
+                            <?php echo wpautop($left_content); ?>
+                        </div>
+                    </div>
+                </div>
+                <?php if( !empty($right_content) || !empty($right_content) ) : ?>
+                <div class="col-md-6">
+                    <div class="thumbnail end-box">
+                        <h2><?php echo $right_title; ?></h2>
+                        <div class="img-box">
+                            <?php echo wpautop($right_content); ?>
+                        </div>
+                    </div>
+                </div>
+                <?php endif; ?>
+            </div>
+        </section>
+        <?php endif; ?>
+    </div>
+</main>
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -2,36 +2,38 @@
 get_header();
 get_template_part( 'breadcrumb' ); ?>
 <main role="main">
-	<section class="container">
-		<div class="row">
-			<h1><?php the_title() ?></h1>
-			<div class="document-viewer feature-img">
-				<?php
+    <section class="container">
+        <div class="row">
+            <h1><?php the_title() ?></h1>
+            <div class="document-viewer feature-img">
+                <?php
 				$page_id = $post->ID;
 				$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
 				$childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
 				?>
-				<a href="<?php echo make_path_relative_no_pre_path($image[0]); ?>" target="_blank">
-					<div>
-						<?php
+                <a href="<?php echo make_path_relative_no_pre_path($image[0]); ?>" target="_blank">
+                    <div>
+                        <?php
 						$image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 						if ( has_post_thumbnail() ) : ?>
-							<img src="<?php echo make_path_relative($image_url[0]); ?>" class="img-responsive" alt="<?php echo $post->post_title ?>">
-						<?php endif; ?>
-					</div>
-				</a>
-				<?php get_image_caption('top'); ?>
-				<div class="overlay">
-					<a target="_blank" class="button align-right" href="<?php echo make_path_relative_no_pre_path($image[0]); ?>">View full image</a>
-				</div>
-			</div>
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; endif; ?>
-		</div>
-	</section>
+                        <img src="<?php echo make_path_relative($image_url[0]); ?>" class="img-responsive"
+                            alt="<?php echo $post->post_title ?>">
+                        <?php endif; ?>
+                    </div>
+                </a>
+                <?php get_image_caption('top'); ?>
+                <div class="overlay">
+                    <a target="_blank" class="button align-right"
+                        href="<?php echo make_path_relative_no_pre_path($image[0]); ?>">View full image</a>
+                </div>
+            </div>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
 
-	<?php
+    <?php
 		$page_id = $post->ID;
 		$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
 		$args = array(
@@ -43,50 +45,52 @@ get_template_part( 'breadcrumb' ); ?>
 		);
 	$the_query = new WP_Query( $args );
 	if ( $the_query->have_posts() ) : ?>
-	<!-- while ( $the_query->have_posts() ) : $the_query->the_post(); -->
-	<section class="container explore-records">
-		<div class="row">
-			<h2>Explore the records</h2>
-			<span id="slider-prev"></span>
-			<span id="slider-next"></span>
-			<div class="bxslider">
-				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-				<a href="<?php echo make_path_relative(get_page_link()); ?>">
-					<div class="document-slide-thumb" style="background-image: url('<?php
+
+    <section class="container explore-records">
+        <div class="row">
+            <h2>Explore the records</h2>
+            <span id="slider-prev"></span>
+            <span id="slider-next"></span>
+            <div class="bxslider">
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                <a href="<?php echo make_path_relative(get_page_link()); ?>">
+                    <div class="document-slide-thumb" style="background-image: url('<?php
 					$page_id = $post->ID;
 					$image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
-					//$childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
+					
 					if (has_post_thumbnail($page_id)) {
 						echo make_path_relative_no_pre_path($image[0]);
 
 					}
 					?>')">
-					</div>
-					<div><p><?php the_title(); ?></p></div>
-				</a>
-				<?php endwhile; ?>
-			</div>
-		</div>
-	</section>
-	<?php endif; wp_reset_postdata(); ?>
-	<section class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<?php
+                    </div>
+                    <div>
+                        <p><?php the_title(); ?></p>
+                    </div>
+                </a>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
+    <?php endif; wp_reset_postdata(); ?>
+    <section class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
 				global $post;
 				$parent = $post->post_parent;
 				$home_title = get_the_title( get_option('page_on_front') );
 				if (is_page() && $parent) : ?>
-					<a class="button align-right" href="<?php echo make_path_relative(get_permalink( $parent )); ?>">
-						<?php echo 'Return to '.get_the_title($parent); ?>
-					</a>
-				<?php else : ?>
-					<a class="button align-right" href="<?php echo make_path_relative(get_site_url()); ?>">
-						<?php echo 'Return to '.$home_title; ?>
-					</a>
-				<?php endif; ?>
-			</div>
-		</div>
-	</section>
+                <a class="button align-right" href="<?php echo make_path_relative(get_permalink( $parent )); ?>">
+                    <?php echo 'Return to '.get_the_title($parent); ?>
+                </a>
+                <?php else : ?>
+                <a class="button align-right" href="<?php echo make_path_relative(get_site_url()); ?>">
+                    <?php echo 'Return to '.$home_title; ?>
+                </a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
 </main>
 <?php get_footer(); ?>
